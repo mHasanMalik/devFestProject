977adfdedabe5d55f82419f2f9be303c2af962e3
mHasanMalik <hassan.100052@gmail.com>
2018-03-02 01:03:29 +0500

    Initial Commit

---
+++ app/src/main/java/com/project/Devlot/tripcast/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.project.Devlot.tripcast.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.Devlot.tripcast.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

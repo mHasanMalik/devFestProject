109873d8347f05b19f6b948019a76201e591e838
AbdullahSheikhh <abdullah.sheikh69@hotmail.com>
2018-03-02 17:35:40 +0500

    1st Commit
    This includes SignUp and SignIn Activities and layouts.

--- app/src/main/java/com/project/Devlot/tripcast/Activity/Sign_inActivity.java
+++ app/src/main/java/com/project/Devlot/tripcast/Activity/Sign_inActivity.java
@@ -2,6 +2,7 @@ package com.project.Devlot.tripcast.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.project.Devlot.tripcast.R;
@@ -9,6 +10,7 @@ import com.project.Devlot.tripcast.R;
 public class Sign_inActivity extends AppCompatActivity {
 
     EditText email, password;
+    Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,7 @@ public class Sign_inActivity extends AppCompatActivity {
     public void initializeWidgets(){
         email = (EditText) findViewById(R.id.eT_email_signIn_xml);
         password = (EditText) findViewById(R.id.eT_password_signIn_xml);
+        signIn = (Button) findViewById(R.id.btn_signIn_xml);
 
     }
 }
